be2864b55426906b3355765495b6deae5d6b8982
Jay Spendlove <jayaloha@hotmail.com>
2022-09-09 15:22:39 -0700

    Added projectFactory function

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-// Factory function for creating a 'todo'
-const todoFactory = (title, description, dueDate, priority) => {
+// Factory function for creating a 'project', a list of 'todo-items'
+const projectFactory = (title) => {
+    // Array of Todo Items that will be assigned to this project
+    let projectTodos = [];
+    const sayHello = () => console.log('Hello! I am project ' + title);
+    return { title, projectTodos, sayHello };
+}
+
+// Factory function for creating a 'todo-item'
+const todoItemFactory = (title, description, dueDate, priority) => {
     const sayHello = () => console.log('Hello! I am todo item ' + title +
     '. My description is ' + description + ', my Due Date is ' + dueDate + 
     ', and my Priority is ' + priority);
     return { title, description, dueDate, priority, sayHello };
 }
 
-var todo = todoFactory('Project', 'The project that I need to do', 
+// My Testing Code Area
+var project = projectFactory('Project 1');
+var todo = todoItemFactory('Create action plan', 'The project that I need to do', 
 '09.27.22', 'High');
+project.sayHello();
 todo.sayHello();
