35baa42afd0e42d62dcd678d7385a2804ffc7406
Jay Spendlove <jayaloha@hotmail.com>
2022-09-12 14:42:25 -0700

    Added a project array for testing and a starting, default project. Created projects now create a DOM element on the display.

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 // Factory function for creating a 'project', a list of 'todo-items'
 const projectFactory = (title) => {
+    info = () => 'Hello! I am project ' + title;
     // Array of Todo Items that will be assigned to this project
     let projectTodos = [];
-    const sayHello = () => console.log('Hello! I am project ' + title);
-    return { title, projectTodos, sayHello };
+    projects.push(this);
+    const project = document.createElement('div');
+    project.textContent = info();
+    display.appendChild(project);
+    return { title, projectTodos, info };
 }
 
 // Factory function for creating a 'todo-item'
-const todoItemFactory = (title, description, dueDate, priority) => {
-    const sayHello = () => console.log('Hello! I am todo item ' + title +
+const todoItemFactory = (project, title, description, dueDate, priority) => {
+    const info = () => 'Hello! I am todo item ' + title +
     '. My description is ' + description + ', my Due Date is ' + dueDate + 
-    ', and my Priority is ' + priority);
-    return { title, description, dueDate, priority, sayHello };
+    ', and my Priority is ' + priority;
+    const todo = document.createElement('div');
+    todo.textContent = info();
+    project.appendChild(todo);
+    return { title, description, dueDate, priority, info };
 }
 
 // My Testing Code Area
-var project = projectFactory('Project 1');
-var todo = todoItemFactory('Create action plan', 'The project that I need to do', 
-'09.27.22', 'High');
-project.sayHello();
-todo.sayHello();
+// var project = projectFactory('Project 1');
+// var todo = todoItemFactory('Create action plan', 'The project that I need to do', 
+// '09.27.22', 'High');
+// project.sayHello();
+// todo.sayHello();
+
+const display = document.querySelector('#display');
+let projects = [];
+var defaultProject = projectFactory('Default Project');
+// var defaultTodo = todoItemFactory(defaultProject, 'Todo-Item1', "Email Boss", 
+// '09.20.22', 'High');
+console.log(projects[0].info());
